2e6b0108e11b76cabbdf23f6dd7a907d76612024
Bellilrihab <74023936+Bellilrihab@users.noreply.github.com>
2020-11-18 19:10:14 +0100

    gameover

---
+++ BallGame/Assets/Script/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool GameEnded=false;
+  public void EndGame()
+    {
+        if (GameEnded == false)
+        {
+            GameEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", 2f);
+        }
+   }
+
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
---
+++ BallGame/Assets/Script/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public Mvt BallMvt;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Ball")
+        {
+            Debug.Log("GameOver");
+
+            BallMvt.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
